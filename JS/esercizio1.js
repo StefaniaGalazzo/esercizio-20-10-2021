82c6cd5127f5f6513e6ec648e58c11417bebf5ca
Stefania Galazzo <galazzostefania@gmail.com>
2021-10-21 09:51:57 +0200

    upload esercizio1

--- JS/esercizio1.js
+++ JS/esercizio1.js
@@ -23,41 +23,7 @@ console.log(array);
 
 
 
-/*
-
-
-
-
-
-*/
-
-
-
-
-
-
-
-
 /* risultato in console 
-
-
-let str = prompt("Inserisci 5 nomi separati da una virgola", `Stefania,Sandro,Maria,Giacomo,Giorgia`).split(',');
-
-str.unshift (`${str.splice(3,1,'Marta')}`);
-str.push('Franco','Valentina',);
-
-let arrAge = [32,21,47,12,78];
-arrAge.unshift(arrAge.splice(3,1,6)[0]);
-arrAge.push(54,32);
-
-let arrPush = [str, arrAge];
-
-console.log(arrPush);
-
-
-
-
-
 
 [
     [“Giacomo”, “Stefania”, “Sandro”, “Maria”, “Marta”, “Giorgia”, “Franco”, Valentina],
